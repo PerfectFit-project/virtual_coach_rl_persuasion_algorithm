0486a46d6c7d439dae1c2dc7917177a59a0fd17f
Nele Albers <nele.96@web.de>
2021-01-22 15:04:00 +0100

    Update Jan 22

--- db_scripts/init_db.py
+++ db_scripts/init_db.py
@@ -30,10 +30,12 @@ try:
                                 state_2 TEXT,
                                 state_3 TEXT,
                                 state_4 TEXT,
-								action_type_index_list TEXT,
-                                group INTEGER
-                                );'''
-
+                                action_type_index_list TEXT,
+                                study_group INTEGER,
+                                user_satisfaction2 TEXT,
+                                user_satisfaction4 TEXT
+                                )'''
+								
     cursor = sqliteConnection.cursor()
     print("Successfully connected to SQLite")
     #cursor.execute('''DROP table users;''')
@@ -47,7 +49,11 @@ try:
     data_tuple = ('222', 2, 'droopy|happy', "sdd", " ", " ", "2|9", "1|2", "0|4|1|4|1|2|1|1|3|0", "2|3","2|0|2|0|0|0|3|0|4|0", "0")
     sqlite_query = """INSERT INTO users (id, sessions_done, mood_list, action_planning_answer0, attention_check_list, attention_check_2_list, activity_index_list, action_index_list, state_0, action_type_index_list, state_1, reward_list) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""" 
     cursor.execute(sqlite_query, data_tuple) 
-    sqliteConnection.commit()            
+    sqliteConnection.commit()    
+    data_tuple = ('333', 2, 'droopy|happy', "sdd", " ", " ", "2|9", "1|2", "1|3|1|2|1|0|0|1|0|0", "1|1","4|1|1|0|1|0|2|0|3|0", "1")
+    sqlite_query = """INSERT INTO users (id, sessions_done, mood_list, action_planning_answer0, attention_check_list, attention_check_2_list, activity_index_list, action_index_list, state_0, action_type_index_list, state_1, reward_list) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""" 
+    cursor.execute(sqlite_query, data_tuple) 
+    sqliteConnection.commit()    	
     cursor.close()
 
 except sqlite3.Error as error:
