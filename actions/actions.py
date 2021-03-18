888f72435fbb9dc65ac0eacba6d3e4c795394255
Nele Albers <nele.96@web.de>
2021-03-18 19:51:22 +0100

    all reminder questions

--- actions/actions.py
+++ actions/actions.py
@@ -3,7 +3,6 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
-
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
